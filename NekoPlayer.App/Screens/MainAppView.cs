ce1b4d6e1ce70ae8652ad9322e8f932768ff591d
BoomboxRapsody <seonghyeon802@gmail.com>
2026-05-31 00:38:03 +0900

    Update MainAppView.cs

--- NekoPlayer.App/Screens/MainAppView.cs
+++ NekoPlayer.App/Screens/MainAppView.cs
@@ -3069,22 +3069,15 @@ namespace NekoPlayer.App.Screens
                                 }
                             }
                         },
-                        editPlaylistOverlay = new OverlayContainer
+                        editPlaylistOverlay = new BottomOverlayContainer
                         {
                             Size = new Vector2(1f, 1f),
                             Width = 450,
                             Height = 250,
-                            CornerRadius = NekoPlayerApp.UI_CORNER_RADIUS,
+                            CornerRadius = new CornersInfo(NekoPlayerApp.UI_CORNER_RADIUS, 0, NekoPlayerApp.UI_CORNER_RADIUS, 0),
                             Masking = true,
-                            Origin = Anchor.Centre,
-                            Anchor = Anchor.Centre,
-                            EdgeEffect = new osu.Framework.Graphics.Effects.EdgeEffectParameters
-                            {
-                                Type = osu.Framework.Graphics.Effects.EdgeEffectType.Shadow,
-                                Colour = Color4.Black.Opacity(0.25f),
-                                Offset = new Vector2(0, 2),
-                                Radius = 16,
-                            },
+                            Origin = Anchor.BottomCentre,
+                            Anchor = Anchor.BottomCentre,
                             Children = new Drawable[]
                             {
                                 new Box
@@ -3094,8 +3087,8 @@ namespace NekoPlayer.App.Screens
                                 },
                                 new AdaptiveSpriteText
                                 {
-                                    Origin = Anchor.TopLeft,
-                                    Anchor = Anchor.TopLeft,
+                                    Origin = Anchor.TopCentre,
+                                    Anchor = Anchor.TopCentre,
                                     Text = NekoPlayerStrings.EditPlaylist,
                                     Margin = new MarginPadding(16),
                                     Font = NekoPlayerApp.DefaultFont.With(size: 30, weight: "Bold"),
