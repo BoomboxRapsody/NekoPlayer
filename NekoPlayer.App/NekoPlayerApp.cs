179df54f0f9046b2fb7f3ba3d9366541fe7cf032
BoomboxRapsody <seonghyeon802@gmail.com>
2026-07-03 18:42:57 +0900

    what

--- NekoPlayer.App/NekoPlayerApp.cs
+++ NekoPlayer.App/NekoPlayerApp.cs
@@ -59,7 +59,7 @@ namespace NekoPlayer.App
 
         public static FontUsage Roboto = FontUsage.Default.With("Roboto", 16, "Regular");
 
-        public static string KnownCodename = "Pastel";
+        public static string KnownCodename = "Pastel Yomi";
 
         private BindableNumber<double> sampleVolume = null!;
         private FPSCounter fpsCounter;
