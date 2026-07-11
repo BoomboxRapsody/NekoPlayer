01360cb684e8ef255786cb266c5ade5ef62ad8ba
BoomboxRapsody <seonghyeon802@gmail.com>
2026-07-11 16:43:49 +0900

    Update AudioEffectsConfigManager.cs

--- NekoPlayer.App/Config/AudioEffectsConfigManager.cs
+++ NekoPlayer.App/Config/AudioEffectsConfigManager.cs
@@ -35,7 +35,7 @@ namespace NekoPlayer.App.Config
 
             SetDefault(AudioEffectsSetting.EchoEnabled, false);
             SetDefault(AudioEffectsSetting.EchoDryMix, 2f, 0f, 4f, 0.01f);
-            SetDefault(AudioEffectsSetting.EchoWetMix, 2f, 0f, 4f, 0.01f);
+            SetDefault(AudioEffectsSetting.EchoWetMix, 3f, 0f, 4f, 0.01f);
             SetDefault(AudioEffectsSetting.EchoFeedback, 1f, 0f, 2f, 0.01f);
             SetDefault(AudioEffectsSetting.EchoDelay, 0f, 0f, 6f, 0.01f);
 
@@ -47,7 +47,7 @@ namespace NekoPlayer.App.Config
 
             SetDefault(AudioEffectsSetting.ChorusEnabled, false);
             SetDefault(AudioEffectsSetting.ChorusDryMix, 2f, 0f, 4f, 0.01f);
-            SetDefault(AudioEffectsSetting.ChorusWetMix, 2f, 0f, 4f, 0.01f);
+            SetDefault(AudioEffectsSetting.ChorusWetMix, 3f, 0f, 4f, 0.01f);
             SetDefault(AudioEffectsSetting.ChorusFeedback, 1f, 0f, 2f, 0.01f);
             SetDefault(AudioEffectsSetting.ChorusMinSweep, 1f, 0f, 6000f, 1f);
             SetDefault(AudioEffectsSetting.ChorusMaxSweep, 400f, 0f, 6000f, 1f);
