1bb15a98cdb82d0c5050f2ceef49769fa8fa6149
BoomboxRapsody <seonghyeon802@gmail.com>
2026-07-24 09:58:01 +0900

    Update IMediaPlayer2.cs

--- NekoPlayer.Desktop/Linux/IMediaPlayer2.cs
+++ NekoPlayer.Desktop/Linux/IMediaPlayer2.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) 2026 BoomboxRapsody <boomboxrapsody@gmail.com>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Tmds.DBus;
 
