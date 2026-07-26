255c76a53917f751885a12a6a767736b59dc42eb
BoomboxRapsody <seonghyeon802@gmail.com>
2026-07-26 11:56:19 +0900

    changed some things

--- NekoPlayer.App/Graphics/Shaders/ICustomizedShader.cs
+++ NekoPlayer.App/Graphics/Shaders/ICustomizedShader.cs
@@ -1,9 +1,7 @@
 ﻿// Copyright (c) 2026 BoomboxRapsody <boomboxrapsody@gmail.com>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using osu.Framework.Graphics.Rendering;
-using osu.Framework.Graphics.Shaders;
 
 namespace NekoPlayer.App.Graphics.Shaders
 {
@@ -30,14 +28,6 @@ namespace NekoPlayer.App.Graphics.Shaders
         /// Whether this shader is currently bound.
         /// </summary>
         bool IsBound { get; }
-
-        /// <summary>
-        /// Retrieves a uniform from the shader.
-        /// </summary>
-        /// <param name="name">The name of the uniform.</param>
-        /// <returns>The retrieved uniform.</returns>
-        Uniform<T> GetUniform<T>(string name)
-            where T : unmanaged, IEquatable<T>;
 
         /// <summary>
         /// Binds an <see cref="IUniformBuffer"/> to a uniform block of the given name.
