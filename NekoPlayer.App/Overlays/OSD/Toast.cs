deaca7add67a4fb1b7508797c31c984b9c080a2e
BoomboxRapsody <seonghyeon802@gmail.com>
2026-07-29 18:32:20 +0900

    changed some things

--- NekoPlayer.App/Overlays/OSD/Toast.cs
+++ NekoPlayer.App/Overlays/OSD/Toast.cs
@@ -98,7 +98,7 @@ namespace NekoPlayer.App.Overlays.OSD
         {
             descriptionText.Origin = descriptionText.Anchor = (string.IsNullOrEmpty(extraText.Text.ToString())) ? Anchor.CentreLeft : Anchor.TopLeft;
             descriptionText.Colour = ValueSpriteText.Colour = overlayColourProvider.Content2;
-            background.Colour = overlayColourProvider.Background5;
+            background.Colour = overlayColourProvider.Background4;
             extraText.Colour = overlayColourProvider.Background1;
         }
     }
