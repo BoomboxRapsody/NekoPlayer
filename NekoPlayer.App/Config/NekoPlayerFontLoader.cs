1c13388f3154a41ca679768dce545c1c28a721bb
BoomboxRapsody <seonghyeon802@gmail.com>
2026-08-11 19:29:19 +0900

    Update NekoPlayerFontLoader.cs

--- NekoPlayer.App/Config/NekoPlayerFontLoader.cs
+++ NekoPlayer.App/Config/NekoPlayerFontLoader.cs
@@ -242,6 +242,7 @@ namespace NekoPlayer.App.Config
             game.AddOutlineFont(Resources, @"Fonts/UIFonts/Griun_Mongtori");
             game.AddOutlineFont(Resources, @"Fonts/UIFonts/ONE_Mobile_POP");
             game.AddOutlineFont(Resources, @"Fonts/UIFonts/HayuFont");
+            game.AddOutlineFont(Resources, @"Fonts/UIFonts/DreamHeumulKR");
         }
     }
 }
