fa2c2a0e3f171eaa865fa6afd0667746ce4eb3ba
Mayo_0x0 <seonghyeon802@gmail.com>
2026-08-13 21:43:01 +0900

    Update NekoPlayer.App/Graphics/UserInterface/PushNotificationOverlay.cs
    Co-authored-by: codefactor-io[bot] <47775046+codefactor-io[bot]@users.noreply.github.com>

--- NekoPlayer.App/Graphics/UserInterface/PushNotificationOverlay.cs
+++ NekoPlayer.App/Graphics/UserInterface/PushNotificationOverlay.cs
@@ -51,7 +51,7 @@ namespace NekoPlayer.App.Graphics.UserInterface
                         Direction = FillDirection.Vertical,
                         Padding = new MarginPadding(8),
                         Spacing = new Vector2(8),
-                        Children = new Drawable[]
+                        Children = Array.Empty<Drawable>()
                         {
                         }
                     }
