df1a8f6fc64b167ddfb9c5d18cee921da61f4ee2
BoomboxRapsody <seonghyeon802@gmail.com>
2026-08-30 20:57:23 +0900

    Update NekoPlayerConfigManager.cs

--- NekoPlayer.App/Config/NekoPlayerConfigManager.cs
+++ NekoPlayer.App/Config/NekoPlayerConfigManager.cs
@@ -71,7 +71,7 @@ namespace NekoPlayer.App.Config
             SetDefault(NekoPlayerSetting.AccessToken, string.Empty);
             SetDefault(NekoPlayerSetting.AudioQuality, AudioQuality.PreferHighQuality);
             SetDefault(NekoPlayerSetting.CloseButtonAction, CloseButtonAction.HideToTrayIcon);
-            SetDefault(NekoPlayerSetting.ColourScheme, OverlayColourScheme.Purple);
+            SetDefault(NekoPlayerSetting.ColourScheme, OverlayColourScheme.Lime);
             SetDefault(NekoPlayerSetting.ProfileImageShape, ProfileImageShape.Circle);
             SetDefault(NekoPlayerSetting.CommentsSortCriteria, CommentsSortCriteria.Top);
 
