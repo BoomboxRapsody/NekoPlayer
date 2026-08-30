12bf2223a3c7fdcccb72dc8cd61c63393c2f3218
BoomboxRapsody <seonghyeon802@gmail.com>
2026-08-30 18:40:07 +0900

    Update YouTubeAudioStreamDropdown.cs

--- NekoPlayer.App/Graphics/UserInterfaceV3/YouTubeAudioStreamDropdown.cs
+++ NekoPlayer.App/Graphics/UserInterfaceV3/YouTubeAudioStreamDropdown.cs
@@ -62,8 +62,8 @@ namespace NekoPlayer.App.Graphics.UserInterfaceV3
 
                 if (!Current.Disabled && Current.Value == null)
                 {
-                    Current.Value = (IAudioStreamInfo)audioStreamInfo.Where(i => i.IsAudioLanguageDefault == true).GetWithHighestBitrate();
-                    Current.Default = (IAudioStreamInfo)audioStreamInfo.Where(i => i.IsAudioLanguageDefault == true).GetWithHighestBitrate();
+                    Current.Value = (IAudioStreamInfo)audioStreamInfo.GetWithHighestBitrate();
+                    Current.Default = (IAudioStreamInfo)audioStreamInfo.GetWithHighestBitrate();
                 }
             }
             catch (Exception e)
